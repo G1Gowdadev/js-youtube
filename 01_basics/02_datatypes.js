22898a7531c0374ea91d0d14cf88f48d0f69eb96
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-08 13:53:46 +0000

    Datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) we are suing Node.js , not browser
+
+console.log(3 + 3)
+
+console.log("Jeevan")
+
+let name = "hitesh"
+let ageage = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2^^53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof ageage);
