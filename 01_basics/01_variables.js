ceb8134f8a927e4dc0e5bfc466857920a0252f2a
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-08 13:29:14 +0000

    Let Const & Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "Jeevan@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "jeevitha@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
