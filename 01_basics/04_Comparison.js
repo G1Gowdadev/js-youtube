17eb3329e841325d5ed498ffb964a2b1889f28c9
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-13 11:03:15 +0000

    Comparisions +=*-/%

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// Avoid these kind of codes
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// ===
+console.log("2" === 2); // it also checks the dataTypes
+
