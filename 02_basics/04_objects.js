32c1d5ed94a3f3652a34a3681ca60d09a9c7eaef
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-21 12:40:41 +0000

    debuggedCode

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,29 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Jeevan",
+            lastname: "Gowda",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 2: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign(obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
