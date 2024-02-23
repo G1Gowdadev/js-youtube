582f6b7f49fd63e4dd645fb568a475b966498a4a
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-23 19:43:02 +0000

    functions: Calling, Defining, etc.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,5 +40,41 @@ function loginUserMessage(username){
 
 // console.log(loginUserMessage("Jeevan"));
 // OR
-let print = loginUserMessage()
-console.log(print);
+// let print = loginUserMessage()
+// console.log(print);
+
+
+// function calculateCartPrice(...num1) {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = { //defining a function
+    username: "Jeevan",
+    price: 199
+}
+
+function handleObject(anyobject){ //anyobject to user don't get confused to get user.username
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({ //directly passed the values inside the function call
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue (getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 600]));
+
+
+
+
+
+
+
