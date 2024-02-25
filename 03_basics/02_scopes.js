fca786b84e6f356facfe724308f5e07cfaacbfb5
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-25 07:00:17 +0000

    Scopes, functions, Hoisting.

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,34 @@
+// if(true){
+//     let a = 10
+//     const b = 20
+//     'Inner: ${a}'
+//     // var c = 30 // don't use var because it can also print outside the scope
+// }
+
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+// function one () { 
+//     const username = "jeevan"
+
+//     function two (){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+// +++++++++++++++ interesting ++++++++++++++++++
+
+function addone(num) {
+    return num + 1
+}
+console.log(addone(5));
+//Hoisting\\
+const addTwo = fucntion(num) {
+    return num + 2
+}
+addTwo(5)
