f46a8d7676033ae39f2d8060d9a730c576f5b65c
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-26 01:14:01 +0000

    no changes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -6,12 +6,12 @@
 // +++++++1st() for the fucntion definition++++++
 // ++++ 2nd () for the immediate funciton expression++++++
 
-(() =>{ //NAMED IIFE
+(() =>{ // UN-NAMED IIFE
     console.log(`DB CONNNECTED TWO`);
 } ) ();
 
 
-((name) =>{// UN-NAMED IIFE
+((name) =>{//NAMED IIFE
     console.log(`DB CONNNECTED TWO ${name}`);
 } ) ('Jeevan') //passing the value in the 2nd ()
 
