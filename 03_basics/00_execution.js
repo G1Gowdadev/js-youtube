66983affafe27bb15fe3217c9cbc8262014feb32
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-26 11:06:46 +0000

    Execution in JS, Call Stack.

---
+++ 03_basics/00_execution.js
@@ -0,0 +1,33 @@
+/*++++++++ JS EXECUTION CONTEXT ++++++++
+
+===> GLOBAL EXECUTION CONTEXT.
+===> FUNCTION EXECUTION CONTEXT.
+===> EVAL EXECUTION CONTEXT.
+
+--------------------------------------------------
+
+{ } ==> MEMORY CREATION PHASE (CREATION PHASE).
+    ==> EXECUTION PHASE.
+
+---------------------------------------------------
++++++++++++++++++++ 3 PHASES +++++++++++++++++++++
+ 1 => Global Execution => THIS. 
+ 2 => Memory Phase.
+ 3 => Execution Phase.
+
+---------------------------------------------------
+++++++++++++++ Inside Execution Context ++++++++++++
+ [M Phase and E Phase inside Execution context (inside function)]
+ {[Execution context gets Deleted after execution]}
+
+ No. of Function proportional to No. of Execution Context
+ {newVariableEnvironment + executionThread}
+
+ --------------------------------------------------
+++++++++++++++++++ CALL STACK +++++++++++++++++++++
+
+
+
+
+
+*/
