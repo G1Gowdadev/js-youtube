d918fce6cf39c28914a10cef84d2d81772b8586d
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-27 17:57:48 +0000

    While Loop, DoWhile Loop.

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// WHILE LOOP
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`Value of index is ${i}`)
+//     i += 2;
+// }
+
+
+// let myArray = ['flash', 'batman', 'superman']
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+//----------------------------------------------
+// DO-WHILE
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+// IF THE CONDITION IS NOT SATISFIED THEN IT PRINTS 
+// THE VALUE FIRST THEN CHECKS FOR THE CONDITION.
+
+
