f8b562e3ba1864f9c70da30c49e66180df12703a
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-29 09:48:32 +0000

    FOR IN LOOP

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+// FOR IN LOOP = used to return the property of an Object.
+
+const myObject = {
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// This returns the key of the objects.
+// for (const key in myObject) {
+//     console.log(key);
+// }
+// This returns the value of the myobject.
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+//
+for(const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+//FOR IN LOOP ON ARRAY.
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
