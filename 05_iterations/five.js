f92edf1b863c4c06375fdb128f36d44ca62098d5
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-02-29 09:48:03 +0000

    FOR EACH LOOP

---
+++ 05_iterations/five.js
@@ -0,0 +1,24 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function(val) {
+//     console.log(val);
+// })
+
+//-----------------------------------
+//Using ARROW FUNCTION...
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+//--------------------------------
+// Using FUNCTION CALL...
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+//-----------------------------------------
+//Printing the ITEM, INDEX AND THE WHOLE ARRAY.
+coding.forEach ((item, index, arr) => {
+    console.log(item, index, arr);
+})
