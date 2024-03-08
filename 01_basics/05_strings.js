46097abce7aa99377374473b78414e259dbbe015
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-03-08 09:33:01 +0000

    added new useful comments

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,34 +1,37 @@
-const name = "hitesh"
-const repoCount = 50
+// const name = "hitesh"
+// const repoCount = 50
 
-console.log(name + repoCount + " value");
+// console.log(`${name} ${repoCount} value`) //String Interpolation Method.
+// console.log(name + repoCount + " value");
 
 // using `` for String Interpolation
 
-console.log(`Hello my name is ${name} and my repo count is $
-{repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is $
+// {repoCount}`);
 
-const gameName = new String('jeevan-gowda')
+// const gameName = new String('jeevan-gowda')
 
 // console.log(gameName[0]);
-
-
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('v'));
 
-const newString = gameName.substring(0,4)
-console.log(newString);
+//.substring(start, end) => it returns the values as a new string.
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+// console.log(gameName.substring(0,4));
 
-const anotherString = gameName.slice(-8, 4);
-console.log(anotherString);
+//slice(start, end) => it returns the elements of the array according to the parameters
+// const anotherString = gameName.slice(0, 4);
+// console.log(anotherString);
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+//trim() => is used to remove the whitespaces, tabs and newLines.
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
+//replace() => used to replace the a certain character with another.
 const url = "https://jeevan.com/jeevan%20gowda";
-
 console.log((url.replace('%20', '_')));
 console.log((url.includes('kumar')));
