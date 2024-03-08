e9deb5e7b9d86402417cc4acd7c4ce47187b679b
G1Gowdadev <158234345+G1Gowdadev@users.noreply.github.com>
2024-03-08 11:31:30 +0000

    added better comments

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,33 +1,35 @@
-const score = 400;
+// const score = 400;
 
-const balance = new Number(100)
-console.log(balance);
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length); //converts it into a string and then returns the length.
+// console.log(balance.toFixed(2));  //decimal_Place //mostly 2 is used in real life events.
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));   //mostly 2 is used in real life events
+//toPrecision(digit) => 23.845 ==> 24.
+// const otherNumber = 23.845454
+// console.log(otherNumber.toPrecision(2));
 
-const otherNumber = 23.845454
-
-console.log(otherNumber.toPrecision(2));
-
-const hundreds = 1000000;
-// console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));//to convert to indian format (1,00,000)
 
 // ++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++ //
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(5));
-// console.log(Math.floor(0.0));
-// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.abs(-4)); // Used to get the absolute value. here 4
+// console.log(Math.round(4.6)); // Rounds of the value.
+// console.log(Math.ceil(5.5)); // Math.ceil(5.5) == 6
+// console.log(Math.floor(0.5)); // Math.ceil(5.5) == 5
+// console.log(Math.min(4, 3, 6, 8)); // Returns the number with the lowest value.
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10;
 const max = 20;
-
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
 
